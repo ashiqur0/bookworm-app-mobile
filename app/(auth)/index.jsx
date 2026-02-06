47ccaa27e8b4d57ef6177e5ac5575072b200a9d2
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-06 11:47:30 +0600

    Add login screen with illustration and safe area layout

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import style from '../../assets/styles/login.styles'
+import { useState } from 'react';
 
 export default function Login() {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleLoding = () => {
+
+    }
+
+    return (
+        <View style={style.container}>
+            {/* ILLUSTRATION */}
+            <View style={style.topIllustration}>
+                <Image
+                    source={require('../../assets/images/i.png')}
+                    style={style.illustrationImage}
+                    resizeMode='contain'
+                />
+            </View>            
+        </View>
+    );
 }
