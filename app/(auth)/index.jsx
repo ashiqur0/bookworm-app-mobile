75c668e5b8006eb374cbe2415712d1af83f1ad2f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-06 12:45:02 +0600

    Enhance login form with email and password inputs, including visibility toggle and loading state

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -1,6 +1,8 @@
-import { View, Text, Image } from 'react-native'
-import style from '../../assets/styles/login.styles'
+import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import styles from '../../assets/styles/login.styles'
 import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import COLORS from '../../constants/colors';
 
 export default function Login() {
 
@@ -9,20 +11,92 @@ export default function Login() {
     const [showPassword, setShowPassword] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
-    const handleLoding = () => {
+    const handleLogin = () => {
 
     }
 
     return (
-        <View style={style.container}>
+        <View style={styles.container}>
             {/* ILLUSTRATION */}
-            <View style={style.topIllustration}>
+            <View style={styles.topIllustration}>
                 <Image
                     source={require('../../assets/images/i.png')}
-                    style={style.illustrationImage}
+                    style={styles.illustrationImage}
                     resizeMode='contain'
                 />
-            </View>            
-        </View>
-    );
+            </View>
+
+            <View style={styles.card}>
+                <View style={styles.formContainer}>
+                    {/* Email */}
+                    <View style={styles.inputGroup}>
+                        <Text style={styles.label}>Email</Text>
+                        <View style={styles.inputContainer}>
+                            <Ionicons
+                                name="mail-outline"
+                                size={20}
+                                color={COLORS.primary}
+                                style={styles.inputIcon}
+                            />
+
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Enter your email"
+                                placeholderTextColor={COLORS.placeholderText}
+                                value={email}
+                                onChangeText={setEmail}
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                            />
+                        </View>
+                    </View>
+
+                    {/* Password */}
+                    <View style={styles.inputGroup}>
+                        <Text style={styles.label}>Password</Text>
+                        <View style={styles.inputContainer}>
+
+                            {/* LEFT ICON */}
+                            <Ionicons
+                                name="lock-closed-outline"
+                                size={20}
+                                color={COLORS.primary}
+                                style={styles.inputIcon}
+                            />
+
+                            {/* INPUT */}
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Enter your password"
+                                placeholderTextColor={COLORS.placeholderText}
+                                onChangeText={setPassword}
+                                secureTextEntry={!showPassword}
+                            />
+
+                            <TouchableOpacity
+                                onPress={() => setShowPassword(!showPassword)}
+                                style={styles.eyeIcon}
+                            >
+                                <Ionicons
+                                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                                    size={20}
+                                    color={COLORS.primary}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    {/* LOGIN BUTTON */}
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={handleLogin}
+                        disabled={isLoading}
+                    >
+                        {
+                            isLoading ? (<ActivityIndicator color='#fff' />) : (<Text style={styles.buttonText}>Login</Text>)
+                        }
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>);
 }
