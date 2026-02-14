31f149979d21d6923205f784d328aa020222c093
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-14 08:01:33 +0600

    Enhance TabLayout component with safe area insets and improved styling for better navigation experience

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,31 @@
 import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
+import COLORS from '../../constants/colors'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function TabLayout() {
+
+  const insects = useSafeAreaInsets();
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        headerTitleStyle: {
+          color: COLORS.primary,
+          fontWeight: '600',
+        },
+        headerShadowVisible: false,
+
+        tabBarStyle: {
+          backgroundColor: COLORS.cardBackground,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          paddingTop: 5,
+          paddingBottom: insects.bottom, // add bottom inset to padding for safe area
+          height: 60 + insects.bottom, // add bottom inset to height for safe area
+        }
       }}
     >
       <Tabs.Screen
