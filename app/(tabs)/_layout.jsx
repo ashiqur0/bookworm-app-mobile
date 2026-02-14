b9ec21796eedc60e5dd64b89654975f4be8496bc
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-14 08:00:22 +0600

    Refactor TabLayout component to include screen options and tabBar icons for better navigation experience

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,54 @@
+import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
 export default function TabLayout() {
   return (
-    <Tabs>
-        <Tabs.Screen name='index' />
-        <Tabs.Screen name='create' />
-        <Tabs.Screen name='profile' />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="home-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='create'
+        options={{
+          title: 'Create',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="add-circle-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="person-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
